5e3281b583d59ecc37cf078e0394b1b4eaaffebf
Almog <almogbh.l@gmail.com>
2019-08-07 01:36:20 +0300

    Updates

---
+++ precache-manifest.d3766055383073d15888b4090205ea8d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e587e1ab15c20cb4030ae15369e5e236",
+    "url": "/MyLocations/index.html"
+  },
+  {
+    "revision": "98ffbfae6ea9940588a1",
+    "url": "/MyLocations/static/js/2.1b82cf90.chunk.js"
+  },
+  {
+    "revision": "a2317420e71eadbd72f1",
+    "url": "/MyLocations/static/js/main.d6cdc101.chunk.js"
+  },
+  {
+    "revision": "1aecb3441599fdcc0e2d",
+    "url": "/MyLocations/static/js/runtime~main.bed56312.js"
+  }
+]);
